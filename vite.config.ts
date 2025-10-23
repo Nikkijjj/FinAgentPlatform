9b29bdaa40bf77ad49a2ae08f22b622ced9ad2d9
肖鑫 <2543920310@qq.com>
2025-10-24 00:27:43 +0800

    更改host

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
     },
     server: {
-      host: true,
+      host: '0.0.0.0',
       port: 8000,
       proxy: {
         '/api': {
