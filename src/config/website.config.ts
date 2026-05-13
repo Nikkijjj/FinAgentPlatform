7c587de3b237d8ea12b2b9be1deb8eec86332856
coolBreeze <104212497+Dmydeloved@users.noreply.github.com>
2026-05-13 10:10:45 +0800

    Update website.config.ts

--- src/config/website.config.ts
+++ src/config/website.config.ts
@@ -2,7 +2,7 @@ import logoImage from '@/assets/images/logo.png';
 import loginImage from '@/assets/images/account-logo.png';
 
 export const websiteConfig = Object.freeze({
-  title: '基于Agent的智能盯盘系统',
+  title: '盯盘助手',
   logo: logoImage,
   loginImage: loginImage,
   loginDesc: '多功能金融领域AI助理',
